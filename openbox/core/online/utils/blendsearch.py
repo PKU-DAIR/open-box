9fa964df4d7420eb30572c79c75fd54db449e0c5
Ezio_csm <Ezio_csm@qq.com>
2022-09-26 19:17:15 +0800

    Update online test

--- openbox/core/online/utils/blendsearch.py
+++ openbox/core/online/utils/blendsearch.py
@@ -29,14 +29,15 @@ class SearchPiece:
 
 class BlendSearchAdvisor(abc.ABC):
     def __init__(self, config_space: ConfigurationSpace,
-                 dead_line=0,
-                 globalsearch=Advisor,
-                 localsearch=CFO,
-                 num_constraints=0,
-                 batch_size=1,
-                 output_dir='logs',
-                 task_id='default_task_id',
-                 random_state=None):
+                 dead_line = 0,
+                 globalsearch = Advisor,
+                 localsearch = CFO,
+                 num_constraints = 0,
+                 batch_size = 1,
+                 pure = False,
+                 output_dir = 'logs',
+                 task_id = 'default_task_id',
+                 random_state = None):
 
         # System Settings.
         self.rng = check_random_state(random_state)
@@ -46,6 +47,7 @@ class BlendSearchAdvisor(abc.ABC):
         # Objectives Settings
         self.u = 1.5
         self.v = 1.0
+        self.pure = pure
         self.dead_line = dead_line
         self.GlobalSearch = globalsearch
         self.LocalSearch = localsearch
@@ -61,7 +63,7 @@ class BlendSearchAdvisor(abc.ABC):
         self.all_configs = set()
 
         # init history container
-        self.history_container = HistoryContainer(task_id, self.num_constraints, config_space=self.config_space)
+        self.history_container = HistoryContainer(task_id, self.num_constraints, config_space = self.config_space)
 
         # Init
         self.cur = None
@@ -106,7 +108,7 @@ class BlendSearchAdvisor(abc.ABC):
 
         return self.history_container.update_observation(observation)
 
-    def get_suggestions(self, batch_size=None):
+    def get_suggestions(self, batch_size = None):
         if batch_size is None:
             batch_size = self.batch_size
 
@@ -115,7 +117,7 @@ class BlendSearchAdvisor(abc.ABC):
     def update_observations(self, observations: List[Observation]):
         return [self.update_observation(o) for o in observations]
 
-    def sample_random_config(self, excluded_configs=None):
+    def sample_random_config(self, excluded_configs = None):
         if excluded_configs is None:
             excluded_configs = set()
 
@@ -135,6 +137,8 @@ class BlendSearchAdvisor(abc.ABC):
         return self.history_container
 
     def select_piece(self):
+        if self.pure:
+            return self.globals
         if self.cur_cnt == self.max_cnt:
             self.cur_cnt = 0
             return self.globals
@@ -187,7 +191,7 @@ class BlendSearchAdvisor(abc.ABC):
         else:
             return self.u * s.perf + self.v * s.cost
 
-    def next(self, config_a: Configuration, delta=0.05, gaussian=False, recu=0):
+    def next(self, config_a: Configuration, delta = 0.05, gaussian = False, recu = 0):
         arr = config_a.get_array().copy()
         d = np.random.randn(*arr.shape)
         if not gaussian:
@@ -203,7 +207,7 @@ class BlendSearchAdvisor(abc.ABC):
             elif isinstance(hp_type, NumericalHyperparameter):
                 arr[i] = min(arr[i] + d[i], 1.0)
 
-        ret = Configuration(self.config_space, vector=arr)
+        ret = Configuration(self.config_space, vector = arr)
         if ret in self.all_configs:
             if recu > 100:
                 self.logger.warning('Cannot sample non duplicate configuration after %d iterations.' % 100)
