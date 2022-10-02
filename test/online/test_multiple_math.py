850230a088ac1bcd19a1e5b62aac07b76f97d211
zer0M1nd <735790901@qq.com>
2022-10-02 13:54:56 +0800

    update

--- test/online/test_multiple_math.py
+++ test/online/test_multiple_math.py
@@ -4,7 +4,6 @@ import sys
 import time
 import argparse
 
-from test.online.synthetic2 import Schwefel, Rastrigin, Griewank
 
 sys.path.insert(0, ".")
 
@@ -16,10 +15,10 @@ import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-from ConfigSpace import Constant, Configuration
+from ConfigSpace import Constant, Configuration, UniformFloatHyperparameter, ConfigurationSpace
 from sklearn.model_selection import train_test_split
 
-from openbox.benchmark.objective_functions.synthetic import Ackley, Rosenbrock, Keane
+from openbox.benchmark.objective_functions.synthetic import Ackley, Rosenbrock, Keane, BaseTestProblem
 from openbox import Advisor, sp, Observation, get_config_space, get_objective_function
 
 # Define Objective Function
@@ -35,6 +34,81 @@ try:
     from tqdm import trange
 except ModuleNotFoundError:
     trange = range
+
+
+class Schwefel(BaseTestProblem):
+    r"""Generalized Schwefel's Problem 2.26.
+
+    d-dimensional function (usually evaluated on `[-500, 500]^d`):
+
+        f(x) = \sum_{i=1}^{d} -x_i sin(\sqrt{\abs{x_i}})
+
+    """
+
+    def __init__(self, dim=2, noise_std=0, random_state=None):
+        self.dim = dim
+        params = {'x%d' % i: (-500.0, 500.0, 100.0) for i in range(1, 1 + self.dim)}
+        config_space = ConfigurationSpace()
+        config_space.add_hyperparameters([UniformFloatHyperparameter(k, *v) for k, v in params.items()])
+        super().__init__(config_space, noise_std,
+                         optimal_value=-12596.5,
+                         random_state=random_state)
+
+    def _evaluate(self, X):
+        result = dict()
+        result['objs'] = [np.sum(-X * np.sin(np.sqrt(np.abs(X))), axis=-1)]
+        return result
+
+
+class Rastrigin(BaseTestProblem):
+    r"""Generalized Rastrigin's Function
+
+    d-dimensional function (usually evaluated on `[-5.12, 5.12]^d`):
+
+        f(x) = \sum_{i=1}^{d} (x_i^2 - 10 \cos(2\pi x) + 10)
+
+    """
+
+    def __init__(self, dim=2, noise_std=0, random_state=None):
+        self.dim = dim
+        params = {'x%d' % i: (-5.12, 5.12, 1) for i in range(1, 1 + self.dim)}
+        config_space = ConfigurationSpace()
+        config_space.add_hyperparameters([UniformFloatHyperparameter(k, *v) for k, v in params.items()])
+        super().__init__(config_space, noise_std,
+                         optimal_value=0,
+                         random_state=random_state)
+
+    def _evaluate(self, X):
+        result = dict()
+        result['objs'] = [np.sum(X ** 2 - 10 * np.cos(2 * np.pi * X) + 10, axis=-1)]
+        return result
+
+
+class Griewank(BaseTestProblem):
+    r"""Generalized Griewank's Function
+
+    d-dimensional function (usually evaluated on `[-600, 600]^d`):
+
+        f(x) = 1/4000 \sum_{i=1}^{d} - \prod_{i=1}^d \cos(x_i / \sqrt{i}) + 1
+
+    """
+
+    def __init__(self, dim=2, noise_std=0, random_state=None):
+        self.dim = dim
+        params = {'x%d' % i: (-600, 600, 100) for i in range(1, 1 + self.dim)}
+        config_space = ConfigurationSpace()
+        config_space.add_hyperparameters([UniformFloatHyperparameter(k, *v) for k, v in params.items()])
+        super().__init__(config_space, noise_std,
+                         optimal_value=0,
+                         random_state=random_state)
+
+    def _evaluate(self, X):
+        result = dict()
+        result['objs'] = [np.sum(X ** 2, axis=-1) / 4000.0 -
+                          np.prod(np.cos(X / (np.arange(1, self.dim - 1)) ** 0.5)) + 1]
+        return result
+
+
 
 FUNCTIONS = [
     Schwefel(dim=12),
