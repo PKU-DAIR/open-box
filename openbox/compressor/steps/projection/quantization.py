652b58d835258130da21705daf0d20a8699e759b
Lingching Tung <1355532189@qq.com>
2025-12-20 15:12:24 +0000

    feat: toggle of adaptive quantization

--- openbox/compressor/steps/projection/quantization.py
+++ openbox/compressor/steps/projection/quantization.py
@@ -14,7 +14,7 @@ class QuantizationProjectionStep(TransformativeProjectionStep):
     def __init__(self, 
                  method: str = 'quantization',
                  max_num_values: int = 10,
-                 seed: int = 42,
+                 seed: int = 42, adaptive: bool = False,
                  **kwargs):
         super().__init__(method=method, **kwargs)
         self._max_num_values = max_num_values
@@ -22,6 +22,7 @@ class QuantizationProjectionStep(TransformativeProjectionStep):
         self._rs = np.random.RandomState(seed=seed)
         
         self._knobs_scalers: dict = {}
+        self.adaptive = adaptive
     
     def _build_projected_space(self, input_space: ConfigurationSpace) -> ConfigurationSpace:
         self._knobs_scalers = {}
@@ -149,7 +150,7 @@ class QuantizationProjectionStep(TransformativeProjectionStep):
         return quantized_dict
     
     def supports_adaptive_update(self) -> bool:
-        return True
+        return self.adaptive
     
     def update(self, progress: OptimizerProgress, history: History) -> bool:
         # Stagnant: reduce quantization (increase max_num_values) to expand search space
@@ -169,4 +170,3 @@ class QuantizationProjectionStep(TransformativeProjectionStep):
                 return True
         
         return False
-
