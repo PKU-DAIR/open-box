73dc8104eefd1bdc1b1308d813edf016f340ce3c
Lingching Tung <1355532189@qq.com>
2026-01-07 20:31:42 +0800

    fix: improve TL surrogate robustness for MF data edge cases

--- openbox/surrogate/tlbo/mfgpe.py
+++ openbox/surrogate/tlbo/mfgpe.py
@@ -28,9 +28,14 @@ class MFGPE(BaseTLSurrogate):
         self.iteration_id = 0
 
     def update_mf_trials(self, mf_hpo_data: List[History]):
-        if self.K == 0:
-            self.K = len(mf_hpo_data) - 1  # K is the number of low-fidelity groups
-            self.w = [1. / self.K] * self.K + [0.]
+        if len(mf_hpo_data) == 0:
+            return
+        if self.K <= 0:
+            self.K = max(len(mf_hpo_data) - 1, 0)
+            if self.K > 0:
+                self.w = [1. / self.K] * self.K + [0.]
+            else:
+                self.w = [1.]
             self.snapshot_w = self.w
         self.source_hpo_data = mf_hpo_data
         # Refit the base surrogates.
